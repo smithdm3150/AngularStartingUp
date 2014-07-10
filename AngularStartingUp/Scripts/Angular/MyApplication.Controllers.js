cfc09ed123feed235de6e5ae1dd028ac4ec8619c
darcy <darcysmith@esdm.co.uk>
2014-07-10 10:37:48 +0100

    Initial Checkin

---
+++ AngularStartingUp/Scripts/Angular/MyApplication.Controllers.js
@@ -0,0 +1,27 @@
+﻿var MyNamespace = MyNamespace || {};
+
+(function() {
+    MyNamespace.MyClass = function(width, height) {
+        // private variables
+        var dimensions = {
+            width: width,
+            height: height
+        };
+
+        // private methods
+        // creating getWidth and getHeight
+        // to prevent access by reference to dimensions
+        var getWidth = function() {
+            return dimensions.width;
+        };
+        var getHeight = function() {
+            return dimensions.height;
+        };
+        // public API
+        return {
+            getWidth: getWidth,
+            getHeight: getHeight
+        };
+    };
+
+});
---
+++ AngularStartingUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularStartingUp.Startup))]
+namespace AngularStartingUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
